aa842649a102f1afceb8de69b32b5f689d3bf1d7
NaoLizanoG <naomilizano@gmail.com>
2023-06-20 12:43:21 -0500

    Reslucion para un estudiante

--- 3-alumnos.c
+++ 3-alumnos.c
@@ -11,25 +11,43 @@ struct persona
     float promedio;
 };
 struct persona alumno1;
+struct persona alumno2;
+struct persona alumno3;
 
-puts("Ingrese el nombre del estudiante");
+printf("\nIngrese el numero de matricula\n");
+scanf("%d",&alumno1.matriculas );
+fflush(stdin);
+
+puts("\nIngrese el nombre del estudiante");
 gets(alumno1.nombre);
+
+fflush(stdin);
 
 puts("\nIngrese la direccion");
 gets(alumno1.direccion);
+fflush(stdin);
 
 puts("\nIngrese la carrera");
 gets(alumno1.carrera);
+fflush(stdin);
 
-printf("\nIngrese el numero de matricula");
-scanf("%d",&alumno1.matriculas );
 
-printf("\nIngrese el numero el promedio");
+
+printf("\nIngrese el numero del promedio\n");
 scanf("%f",&alumno1.promedio );
+fflush(stdin);
 
 
 
-printf("La matricula es: %d", alumno1.matriculas);
+printf("La matricula dle estudainte es: %d\n", alumno1.matriculas);
+puts("El nombre del estudiante es:");
+puts(alumno1.nombre);
+puts("La direccion es:");
+puts(alumno1.direccion);
+puts("La carrera es:");
+puts(alumno1.carrera);
+printf("La nota promedio es: %f\n", alumno1.promedio);
+
 
 
 return 0;
